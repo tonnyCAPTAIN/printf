e9ee7185254726d911158467b1eb8a145f4d2eae
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-12 10:32:28 -0700

    main

--- main.h
+++ main.h
@@ -1,17 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-
-int _putchar(char c);
-
 int _printf(const char *format, ...);
-
-void print_string(char *str);
-void print_int_helper(int n);
-void print_char(va_list ap);
-void print_str(va_list ap);
-void print_percent_sign(va_list ap);
-void print_int(va_list ap);
+int _putchar(char c);
+int print_string(char *);
+int print_decimal(int n);
 
 #endif
