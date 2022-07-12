b9eb12342f4deee53d1d3e0b62622a11ab25e941
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-12 09:43:49 -0700

    test file

---
+++ test.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main- Entry point
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int n = _printf("%u", 2.35);
+	 printf("\nn = %d \n", n);
+
+	 return (0);
+}
