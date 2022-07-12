50b0a63e6d049145b36c0a5007bf8921ac556f43
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-12 04:59:50 -0700

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -1,57 +1,49 @@
+#include <stdarg.h>
 #include "main.h"
-
-/**
- * _printf - Prints output in a given format
- * @format: A string containing characters to be printed
- * Return: total count of the characters printed
- */
-
 int _printf(const char *format, ...)
 {
-	char buffer[1024];
-	int i, j = 0, a = 0, *index = &a;
-	va_list valist;
-	vtype_t spec[] = {
-		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
-		{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
-		{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
-		{'R', format_R}, {'\0', NULL}
-	};
+	int count = 0, i;
 
-	if (!format)
-		return (-1);
-	va_start(valist, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
-		{
-			if (*index == 1024)
-			{
-				_write_buffer(buffer, index);
-				reset_buffer(buffer);
-				*index = 0;
-			}
-			buffer[*index] = format[i];
+	va_list data;
+	va_start(data, format);
+
+	for (i = 0; format[i] != '\0';){
+	/*couint no of characters and print to screen*/
+
+		if (format[i] != '%'){
+		count += _putchar(format[i]);
+		i++;
 		}
-		if (format[i] == '\0')
+		else if (format[i] == '%' && format[i+1] != ' '){
+		switch (format[i + 1]){
+		
+			case 'c':
+				/*printthe character from thr va_arguments*/
+				count += _putchar(va_arg(data, int));
+				break;
+			case 's':
+				count += print_string(va_arg(data, char *));
+				break;
 
-			break;
-		if (format[i] == '%')
-		{
-			i++;
-			for (j = 0; spec[j].tp != '\0'; j++)
-			{
-				if (format[i] == spec[j].tp)
-				{
-					spec[j].f(valist, buffer, index);
-					break;
-				}
-			}
+			case '%':
+				 count += _putchar('%');
+				 break;
+			case 'd':
+				 count += print_decimal(va_arg(data, int));
+				 break;
+			case 'i':
+				  count += print_decimal(va_arg(data, int));
+				  break;
+			default:
+				  break;
+		}
+
+		i += 2;
+		
 		}
 	}
-	va_end(valist);
-	buffer[*index] = '\0';
-	_write_buffer(buffer, index);
-	return (*index);
-}
 
+
+
+	return (count);
+}
