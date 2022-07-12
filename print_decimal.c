43eb5560d3790bf076283c3129ecba062bd22556
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-07-12 10:08:05 -0700

    function that produces output according to a format.

---
+++ print_decimal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int print_decimal(int value)
+{
+	 int count = 0;
+	 if (value < 0) 
+	 {
+		 count += _putchar('-');
+		  value = value * -1;
+	 }
+	  /* First remove the last digit of number and print
+	   * the remaining digits using recursion, then print last digit
+	   */
+
+	  if (value/10)
+		   count += print_decimal(value/10);
+
+	   count += _putchar(value%10 + '0');
+
+	   return (count);
+}
